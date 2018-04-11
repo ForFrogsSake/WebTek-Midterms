8d5b827889760e7a23e594798f0845ba3a1d9985
kiiayla <kyla.kate@gmail.com>
2018-04-11 22:46:26 +0800

    indexeddb script

--- PWA/script.js
+++ PWA/script.js
@@ -74,58 +74,80 @@ function signUp(){
             }
 
 			*/
+//constant called userData 
+const UsersDB = "users";
 
 var db;
 
-
-var idbSupported = false; 
 //created a global variable called idbSupported and 
-	//used it as a flag to check if the browser can use IndexedDB          
+	//used it as a flag to check if the browser can use IndexedDB  
+var idbSupported = false;
+//Check if IndexedDB is supported in window.
+//Used a DOMContentLoaded event to wait for the page to load.        
 document.addEventListener("DOMContentLoaded", function(){
 	if("indexedDB" in window) {
    		idbSupported = true;
     }
-//Check if IndexedDB is supported in window.
-//Used a DOMContentLoaded event to wait for the page to load. 
-             
+            
     if(idbSupported) {
-        var request = indexedDB.open("usersdb",1);
-        //the '1' in the open method is the version of the db which determines the database schema
+    	//the '1' in the open method is the version of the db which determines the database schema
         //the result for the open function is an instance of an IDBDatabase 
-           
+        var request = indexedDB.open("UsersDB",1);
+        
+        //success event triggers the onsuccess() function whose type property is set to "success"     
     	request.onsuccess = function(e) {
             console.log("Success!");
                 db = e.target.result;
         }
-
-        //success event triggers the onsuccess() function whose type property is set to "success"
-             
+		
+		//handle errors 
+        //event error triggers the onerror() function whose type property is set to "error"     
         request.onerror = function(e) {
             console.log("Error");
             console.dir(e);
         }
-
-        //event error triggers the onerror() function whose type property is set to "error" 
-
+       
+        //onupgradeneeded event is triggered when creating a new database or increasing the version number of an existing database. 
         request.onupgradeneeded = function(e){
         	console.log("Running upgrade");
         	var thisDb = e.target.result;
-        }
 
-        //
-             
-    }
+        	//Create a userStore to hold information about our users. 
+        	//username as a keyPath, guaranteed to be unique
+        	var userStore = thisDb.createUserStore("users", {keyPath: "username"});
 
-    var userStore = db.createUserStore("name", {keyPath: "myName"});
+        	//create another user store called 'names'
+        		//autoincrement flag is set as true
+        	var usrStore = this.Db.createUserStore("names", {autoincrement: true});
 
-    const userData = [];
+        	//Create an index to search users by name. There are moments where we could encounter same name
+        		//so we cannot use a unique index. 
+        	userStore.createIndex = ("name", "name", {unique: false});
 
-    const dbName = "NAME";
+        	//Create an index to search users by email. Make sure that no users have the same email
+        		//Use the unique index 
+        	userStore.createIndex = ("email", "email", {unique: true});
 
-    var request = indexedDB.open("userdb", 2);
+        	//Use the transaction.oncomplete to make sure the userStore creation is finished before adding data. 
+        		//oncomplete event handler of IDBTransaction interface handles the complete event,
+        			//it is fired when the transaction successfully completes.
+        	userStore.transaction.oncomplete = function(e){
+        		//Store values on the newly created userStore
+        			//readwrite mode means you can change the data in your database. 
+        		var userDataStore = thisDb.transaction("users", "readwrite").userStore("users");
+        		userData.forEach(function(user) {
+        			userDataStore.add(user);
+        		});
+         	} 
 
-             
+      	}            
+    }            
 },false);
+
+
+
+
+
 
 
 
